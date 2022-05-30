34c12d3aac11cff85bae265d7c482e3a06096f3d
ShavinduAvishka <shavindu.avishka@gmail.com>
2022-05-30 15:56:34 +0530

    firebase conncection

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,12 @@
 import { BellIcon, SearchIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import useAuth from '../hooks/useAuth'
 
 
 function Header() {
     const [isScrolled, setIsScrolled] = useState(false)
+    const { logout } = useAuth()
 
     useEffect(() => {
         const handleScroll = () => {
@@ -47,6 +49,7 @@ function Header() {
             <BellIcon className="h-6 w-6" />
             <Link href="/account">
                 <img
+                onClick={logout}
                 src="https://rb.gy/vahq7p"
                 width={50}
                 height={50}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import Row from '../components/Row'
+import useAuth from '../hooks/useAuth'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
 
@@ -28,6 +29,10 @@ const Home = ({netflixOriginals,
   topRated,
   trendingNow,
 }: Props ) => {
+
+  const { logout, loading } = useAuth()
+
+  if (loading) return null
   
   return (
     <div className="className={`relative h-screen
